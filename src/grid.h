95c2c1783506843c3a0dd4af2a3b03b1f7bb8ba2
saidelbakai <saidelbakai1985@gmail.com>
2024-07-14 20:17:41 +0200

    4

--- src/grid.h
+++ src/grid.h
@@ -1,22 +1,20 @@
 #pragma once
-#include <vector>
 #include <raylib.h>
+#include <vector>
 
 using namespace std;
 
-class Grid
-{
+class Grid {
 private:
-    int numRows;
-    int numCols;
-    int cellSize;
-    vector<Color> colors;
+  int numRows;
+  int numCols;
+  int cellSize;
+  vector<Color> colors;
 
 public:
-    Grid();
-    int grid[20][10];
-    void Initialize();
-    void print();
-    void Draw();
+  Grid();
+  int grid[20][10];
+  void Initialize();
+  void print();
+  void Draw();
 };
-
