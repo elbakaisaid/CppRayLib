ecd6c71ba62367571980eda4a82cb0fc1aa85c04
saidelbakai <saidelbakai1985@gmail.com>
2024-07-14 20:15:47 +0200

    3

--- src/block.h
+++ src/block.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "Position.h"
+#include "colors.h"
 #include <map>
 #include <vector>
-#include "colors.h"
 
 using namespace std;
 
 class block {
 private:
-    int cellSize;
-    int rotationState;
-    vector<Color> colors;
+  int cellSize;
+  int rotationState;
+  vector<Color> colors;
 
-  public:
-    block();
-    void Draw();
-    int id;
-    map<int, vector<Position>> cells;
-    ~block();
+public:
+  block();
+  void Draw();
+  int id;
+  map<int, vector<Position>> cells;
+  ~block();
 };
